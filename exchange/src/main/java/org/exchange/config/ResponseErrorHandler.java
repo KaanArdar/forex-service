2b6885a0c022ff15b65bfca7c079f2b2c96617c0
kaanardar <kaan.ardar@betbull.com>
2023-08-18 16:12:06 +0300

    modified all exception layer

--- exchange/src/main/java/org/exchange/config/ResponseErrorHandler.java
+++ exchange/src/main/java/org/exchange/config/ResponseErrorHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.exchange.client.config.ForexException;
 import org.exchange.dto.ErrorResponse;
 import org.exchange.utility.MessageSourceUtil;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @AllArgsConstructor
 public class ResponseErrorHandler {
 
+    //TODO: can be add BusinessException
     private final MessageSourceUtil messageSourceUtil;
 
     @ExceptionHandler(ForexException.class)
@@ -26,5 +29,13 @@ public class ResponseErrorHandler {
                 .build());
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(ForexException exception) {
+        log.error("Service Exception occurred", exception);
+        return ResponseEntity.ok(ErrorResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(messageSourceUtil.getMessageFromSource(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value())))
+                .build());
+    }
 
 }
